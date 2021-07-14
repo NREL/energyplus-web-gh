a17413487df03c6bb9a9c2d06751f6f60a397599
lin <llin@nrel.gov>
2021-07-14 14:19:22 -0600

    App module and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DownloadsComponent } from './downloads/downloads.component';
+import { DocumentationComponent } from './documentation/documentation.component';
+import { QuickStartComponent } from './quick-start/quick-start.component';
+import { SupportTrainingComponent } from './support-training/support-training.component';
+import { LicensingComponent } from './licensing/licensing.component';
+import { WeatherComponent } from './weather/weather.component';
+import { WeatherRegionComponent } from './weather-region/weather-region.component';
+import { ReferencesComponent } from './references/references.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+}, {
+  path: 'downloads',
+  component: DownloadsComponent
+}, {
+  path: 'documentation',
+  component: DocumentationComponent
+}, {
+  path: 'quick-start',
+  component: QuickStartComponent
+}, {
+  path: 'support-training',
+  component: SupportTrainingComponent
+}, {
+  path: 'licensing',
+  component: LicensingComponent
+}, {
+  path: 'weather',
+  component: WeatherComponent
+}, {
+  path: 'weather-region',
+  component: WeatherRegionComponent
+}, {
+  path: 'references',
+  component: ReferencesComponent
+}, {
+  path: '**',
+  redirectTo: ''
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,61 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { DocumentationComponent } from './documentation/documentation.component';
+import { QuickStartComponent } from './quick-start/quick-start.component';
+import { SupportTrainingComponent } from './support-training/support-training.component'
+import { LicensingComponent } from './licensing/licensing.component';
+import { WeatherComponent } from './weather/weather.component';
+import { RecentReleaseComponent } from './shared/components/recent-release/recent-release.component';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { DownloadsComponent } from './downloads/downloads.component';
+import { ReferencesComponent } from './references/references.component';
+import { ReferenceItemComponent } from './shared/components/reference-item/reference-item.component';
+import { PaginationComponent } from './shared/components/pagination/pagination.component';
+import { faExclamationTriangle, faPen } from '@fortawesome/free-solid-svg-icons';
+import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons';
+import { faBuilding } from '@fortawesome/free-regular-svg-icons';
+import { faTasks } from '@fortawesome/free-solid-svg-icons';
+import { faPenSquare } from '@fortawesome/free-solid-svg-icons';
+import { faPlayCircle } from '@fortawesome/free-regular-svg-icons';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { faChartPie } from '@fortawesome/free-solid-svg-icons';
+import { faChartBar } from '@fortawesome/free-regular-svg-icons';
+import { faFolderOpen } from '@fortawesome/free-regular-svg-icons';
+import { faFile } from '@fortawesome/free-regular-svg-icons';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    DocumentationComponent,
+    QuickStartComponent,
+    SupportTrainingComponent,
+    LicensingComponent,
+    WeatherComponent,
+    RecentReleaseComponent,
+    DownloadsComponent,
+    ReferencesComponent,
+    ReferenceItemComponent,
+    PaginationComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(faGithub, faExclamationTriangle, faArrowCircleRight, faBuilding, faTasks, faPenSquare, faPlayCircle, faClock, faChartPie, faChartBar, faFolderOpen, faFile);
+  }
+}
